56f0cc870facb1dd0691dfe292ab7370ae7dae43
adrien-wicht <108338531+adrien-wicht@users.noreply.github.com>
2025-04-22 10:06:41 +0000

    Deploying to gh-pages from @ adrien-wicht/adrien-wicht.github.io@cd2e45fe0106237e39df2dedde41af66905226a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,13 +490,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=lly9TBsAAAAJ&hl", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
